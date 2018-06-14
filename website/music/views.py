350e9e32235c4ee32384f3d987fd464b3e650712
root <root@09CPU0242L.amritavidya.edu>
2018-06-14 11:27:40 +0530

    song

--- website/music/views.py
+++ website/music/views.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 from django.views import generic
 from . import models
--- website/website/settings.py
+++ website/website/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.media',
+		'django.template.context_processors.media',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -117,12 +117,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/music/list'
+
 
 
